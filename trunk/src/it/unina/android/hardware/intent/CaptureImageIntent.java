63600e3038dc1b8393cc68f1688e96a580efb418
nicola.amatucci@gmail.com <nicola.amatucci@gmail.com@35a61d72-650d-2b1c-aa7c-dbc50a98f7a7>
2012-07-31 11:18:28 +0000

    Aggiunti INTENT per Camera

---
+++ trunk/src/it/unina/android/hardware/intent/CaptureImageIntent.java
@@ -0,0 +1,80 @@
+package it.unina.android.hardware.intent;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+
+//NOTA: getInstrumentation().getContext() per assets
+public class CaptureImageIntent extends Activity {
+	public static int MY_RESULT = RESULT_OK;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	
+    	if ( MY_RESULT == RESULT_OK )
+    	{
+        	//dati ritornati
+        	Intent i = new Intent();
+        		    	
+        	Uri saveUri = null;        	
+	    	if (this.getIntent().getExtras() != null)
+	    		saveUri = this.getIntent().getExtras().getParcelable(it.unina.android.provider.MediaStore.EXTRA_OUTPUT);
+	    
+	    	if (saveUri != null)
+	    	{
+	    		Bitmap photo = this.loadImageFromAsset(getApplicationContext(), "photo.jpg");
+	    		
+	    		OutputStream outputStream = null;
+	    		try
+	    		{
+	    			outputStream = this.getContentResolver().openOutputStream(saveUri);
+		    	    photo.compress(Bitmap.CompressFormat.JPEG, 75, outputStream);
+		    	    outputStream.close();
+		    	    
+		    	    this.setResult(RESULT_OK);
+	    		}
+	    		catch (Exception ex)
+	    		{
+	    			this.setResult(RESULT_CANCELED);
+	    		}
+	    		finally
+	    		{
+					try { if (outputStream != null) outputStream.close(); } catch (IOException ex) {}
+	    		}
+	    	}
+	    	else
+	    	{
+	    		Bitmap preview = this.loadImageFromAsset(getApplicationContext(), "preview.jpg");
+	    		setResult(RESULT_OK, new Intent("inline-data").putExtra("data", preview));	    		
+	    	}	    	
+    	}
+    	else
+    	{
+    		this.setResult(MY_RESULT);
+    	}
+
+    	this.finish();
+    }
+    
+    public Bitmap loadImageFromAsset(Context ctx, String imageName) {
+    	try {
+	    	InputStream in = ctx.getAssets().open(imageName);
+	    	return BitmapFactory.decodeStream(in,null,null);
+    	}
+    	catch(IOException ex) {
+    		
+    	}
+    	return null;
+    }
+    
+}
---
+++ trunk/src/it/unina/android/provider/MediaStore.java
@@ -0,0 +1,8 @@
+package it.unina.android.provider;
+
+public class MediaStore {
+	public static final String ACTION_IMAGE_CAPTURE = "it.unina.android.hardware.intent.CaptureImageIntent";
+	public static final String ACTION_VIDEO_CAPTURE = "it.unina.android.hardware.intent.RecordVideoIntent";
+	
+	public static final String EXTRA_OUTPUT = android.provider.MediaStore.EXTRA_OUTPUT;
+}
